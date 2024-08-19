ce0607fb63993fb4bfc5511bdda5f90cce9bc24e
Pranav <pranav@myrdd.info>
2024-08-19 10:52:01 -0700

    Working version of NLCE, base pipeline model created

--- src/NLCE.jl
+++ src/NLCE.jl
@@ -1,4 +1,49 @@
 module NLCE
 
+include("generation.jl")
+include("tiling.jl")
+include("tagging.jl")
+include("helpers.jl")
+include("summation.jl")
+
+include("ising.jl")
+
+
+# Below is an example of the NLCE process for a square lattice with only nearest neighbors
+# up till order 4 it uses a wrapper function for speed, check the helper functions for 
+# more info
+
+using AlgebraicNumbers: AlgebraicNumber as AN
+using Plots; gr()
+
+## Tasks before next meeting
+# do square, triangle and kagome lattice ising models
+
+## Setting up the lattice geometry
+basis = [[AN(0), AN(0)]]
+primitive_vectors = [[AN(1), AN(0)], [AN(0), AN(1)]]
+final_order = 8
+
+xmax = 2
+total_temp = collect(0.05:0.05:xmax)
+all_energies = []
+
+for order in 7:final_order
+    total_energy = zeros(length(total_temp))
+    nlce_weights = simple_nlce(basis, primitive_vectors, order)
+    for (cluster, mult) in nlce_weights
+        ising_energy = ising_energies(cluster)
+        total_energy += mult .* energy_solver(total_temp, ising_energy) 
+    end
+    push!(all_energies, total_energy)
+end
+
+plot(total_temp, all_energies, label = collect(7:final_order))
+xlims!(0, xmax)
+ylims!(-1, 0)
+title!("Ising Energy for a Square Lattice")
+xlabel!("Temperature")
+ylabel!("Energy")
+savefig("ising_energy.png")
 
 end
--- src/helpers.jl
+++ src/helpers.jl
@@ -1,70 +1,35 @@
-using AlgebraicNumbers
-
-function Base.isless(a::AlgebraicNumber, b::AlgebraicNumber)
-    return (real(a.apprx) < real(b.apprx)) 
-end
-
 """
-    find_bonds(coordinates, neighborhood)
-
-Given a set of coordinates, this function returns a bond array linking each
-coordinate with the corresponding coordinates that are within a specified 
-neighborhood distance from the coordinate, listed by index
-
-# Examples
-```julia-repl
-julia> find_bonds([[0, 0], [1, 1], [0, 1], [1, 0]], 2)
-
-```
 """
-
-function find_bonds(coordinates::Vector{Vector{T}}, neighbors::Int) where {T}
-    magnitude = coord -> sqrt(sum(coord.^2))
-
-    bonds = [Vector{Int}(undef, 0) for i in 1:length(coordinates)]
-    bond_length = [Vector{Int}(undef, 0) for i in 1:length(coordinates)]
-    bond_direction = [Vector{Int}(undef, 0) for i in 1:length(coordinates)]
-
-    directions = Dict{Vector{T}, Int}()
-    
-    # Find the center of all the coordinates
-    center = coordinates[argmin(magnitude.(coordinates .- Ref(sum(coordinates)/length(coordinates))))]
-    # Find all the minimum distances, ignore 0, which there should only be one of,
-    # and then retain only the distances you care about
-    distances = sort(magnitude.(coordinates .- Ref(center)))[2:end]
-    neighborhood = []
-    for distance in distances
-        if !(distance in neighborhood)
-            push!(neighborhood, distance)
-        end
-        if length(neighborhood) == neighbors
-            break
-        end      
-    end    
-
-    # Add all the relevant bonds
-    for (i, coordinate) in enumerate(coordinates)
-        for (j, neighbor) in enumerate(coordinates)
-            for (k, neighborhood_dist) in enumerate(neighborhood)
-                direction = coordinate - neighbor
-                if magnitude(direction) == neighborhood_dist 
-                    push!(bonds[i], j)
-                    push!(bond_length[i], k)
-                    if direction in keys(directions)
-                        push!(bond_direction[i], directions[direction])
-                    else
-                        directions[direction] = length(directions) + 1
-                        push!(bond_direction[i], directions[direction])
-                    end
-                end
-            end
-        end
+function simple_nlce(basis, primitive_vectors, order)
+    all_nlce_clusters = Dict()
+    for nlce_order in 1:order
+      ## Generating the lattice itself
+      lattice, center = generate_lattice(basis, primitive_vectors, 1, nlce_order + 3)
+      
+      # Tile Lattice
+      nlce_clusters = enumerate_subclusters(lattice, nlce_order, [center])
+      
+      # Group Clusters
+      symmetric_reduction = reducing(nlce_clusters, symmetric_tagging)
+      isomorphic_counts = counting(symmetric_reduction, isomorphic_tagging)
+      
+      # Tile Clusters
+      subgraph_counts = Dict() 
+      for (key, (cluster, mult)) in isomorphic_counts
+          subgraph_dict = counting(enumerate_subclusters(cluster, 1, Vector(1:nv(cluster))), isomorphic_tagging)
+          for subgraph_order in 2:(nv(cluster) - 1)
+              merge!(subgraph_dict, counting(enumerate_subclusters(cluster, subgraph_order, Vector(1:nv(cluster))), isomorphic_tagging))
+          end
+          subgraph_counts[key] = (cluster, mult, subgraph_dict)
+      end
+      merge!(all_nlce_clusters, subgraph_counts)
     end
-    (bonds, bond_length, bond_direction)
-end
 
-#x = [[AlgebraicNumber(a), AlgebraicNumber(b)] for a in 0:2 for b in 0:2]
-#x = [[Float64(a), Float64(b)] for a in 0:2 for b in 0:2]
-#println(x)
-#println(find_bonds(x, 2)[1])
-#println(find_bonds(x, 2)[2])
+    final_dict = Dict()
+
+    for (cluster_hash, mult) in nlce_summation(all_nlce_clusters)
+        final_dict[all_nlce_clusters[cluster_hash][1]] = mult
+    end
+
+    final_dict
+end
