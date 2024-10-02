a84458fdd44cd9aaee01f52c4af6f016e0b3871a
Pranav <pranav@myrdd.info>
2024-10-01 20:58:56 -0400

    So much code, defining structs for the NLCE lattice and the pipeline

--- src/pipeline/prune.jl
+++ src/pipeline/prune.jl
@@ -12,8 +12,8 @@ This is step two of the pipeline. In this step, the algorithm takes in
 an array of clusters and a function that is used to categorize the
 clusters. Using this information, the algorithm will collect clusters
 that are alike by the function and combine them to output a hashmap
-containing the hash of the cluster, the (potentially) modified 
-representative cluster and the multiplicity of the cluster. This multiplicity
+containing the hash of the cluster, the permutation needed to modify
+the cluster and the multiplicity of the cluster. This multiplicity
 corresponds to the number of clusters that are identical to the
 representative cluster under the given function.
 """
@@ -26,8 +26,8 @@ Inputs:
       clusters: array of clusters to be pruned
 
       pruning_fxn: function that will be applied to each cluster,
-      needs to return a hash of the cluster and the cluster itself
-      (the function is allowed to modify the cluster)
+      needs to return a hash of the cluster and the permutation 
+      required to convert the cluster to it's isomorphic form
 
 Output:
       Hashmap relating the hash of the cluster to the cluster itself and
@@ -46,7 +46,44 @@ function prune(
 
     for cluster in clusters
         # Find the hash and rearranged cluster for each cluster
-        hash, new_cluster = pruning_fxn(cluster)
+        hash, permutation = pruning_fxn(cluster)
+        # Add this information to the output dictionary
+        cluster_mult[hash] = add_mult_one(get(cluster_mult, hash, 0))
+    end
+
+    cluster_mult
+end
+
+"""
+Permuting version of the main function in step two of the pipeline.
+Collects clusters of the same type and counts their corresponding
+multiplicities. Importantly, this 
+
+Inputs: 
+      clusters: array of clusters to be pruned
+
+      pruning_fxn: function that will be applied to each cluster,
+      needs to return a hash of the cluster and the permutation 
+      required to convert the cluster to it's isomorphic form
+
+Output:
+      Hashmap relating the hash of the cluster to the cluster itself and
+      the corresponding multiplicity of the cluster.
+"""
+function prune_permute(
+        clusters,
+        pruning_fxn
+    )
+
+    # Initialize the empty output dictionary
+    cluster_mult = Dict()
+
+    # Add function for multiplicity
+    add_mult_one = (cluster, mult) -> (cluster, mult + 1)
+
+    for cluster in clusters
+        # Find the hash and rearranged cluster for each cluster
+        hash, permutation = pruning_fxn(cluster)
         # Add this information to the output dictionary
         cluster_mult[hash] = add_mult_one(get(cluster_mult, hash, 0))
     end
---
+++ src/util.jl
@@ -0,0 +1,87 @@
+# Need to do
+#       adj_list_to_adj_matrix:
+#                       - optimize
+
+"""
+General utility functions for a variety of parts of the NLCE process, these
+will generally be math heavy functions that are used often
+"""
+
+"""
+Converts adjacency list for a graph to an adjacency matrix
+"""
+function adj_list_to_adj_matrix(adj_list::AbstractVector{<:AbstractVector{<:Integer}}, values::AbstractVector{<:AbstractVector{<:Integer}})
+
+    # Find the number of vertices in the graph
+    number_vertices = length(adj_list)
+    # Initialize the empty adjacency matrix
+    adj_matrix::Matrix{Int64} = zeros(number_vertices, number_vertices)
+
+    # Loop over all the vertices in the adjacency list
+    for (vertex, neighbors) in enumerate(adj_list)
+        # Loop over all their neighbors
+        for (neighbor_index, neighbor) in enumerate(neighbors)
+            # Add the corresponding value to the adjacency matrix
+            adj_matrix[vertex, neighbor] = values[vertex][neighbor_index]
+        end
+    end
+
+    adj_matrix
+end
+
+function adj_list_to_adj_matrix(adj_list::AbstractVector{<:AbstractVector{<:Integer}})
+
+    # Wrapper function for an adjacency list without weights. This function
+    # will instead add a weight of 1 for every edge
+    values = deepcopy(adj_list)
+    for i in 1:length(values)
+        values[i] .= 1
+    end
+
+    adj_list_to_adj_matrix(adj_list, values)
+end
+
+"""
+Converts adjacency matrix for a graph to an adjacency list
+"""
+function adj_matrix_to_adj_list(adj_matrix::AbstractMatrix{<:Integer})
+
+    # Find the number of vertices in the graph
+    number_vertices = size(adj_matrix)[1]
+    
+    adj_list::Vector{Vector{Int64}} = []
+    
+    for i in 1:number_vertices
+        neighbors = []
+        for j in 1:number_vertices
+            if adj_matrix[i, j] != 0
+                append!(neighbors, j)
+            end
+        end
+        push!(adj_list, neighbors)
+    end
+
+    adj_list
+
+end
+
+function adj_matrix_to_adj_list(adj_matrix::AbstractMatrix{<:Integer}, adj_matrix_weights::AbstractMatrix{<:Integer})
+
+    # Find the number of vertices in the graph
+    number_vertices = size(adj_matrix)[1]
+    
+    adj_list::Vector{Vector{Int64}} = []
+    
+    for i in 1:number_vertices
+        neighbors = []
+        for j in 1:number_vertices
+            if adj_matrix[i, j] != 0
+                append!(neighbors, adj_matrix_weights[i, j])
+            end
+        end
+        push!(adj_list, neighbors)
+    end
+
+    adj_list
+
+end
