657b8c4c69fef90f8854ad1e4fa6af3c5a398696
Pranav <pranav@myrdd.info>
2025-06-16 18:35:16 -0400

    Working on dfs parallel

--- src/Lattices/RealSpaceLattices.jl
+++ src/Lattices/RealSpaceLattices.jl
@@ -1,29 +1,27 @@
 struct RealSpaceLattice{
-        C<:AbstractMatrix,
-        S<:AbstractMatrix{<:Integer},
-        T<:AbstractVector{ExpansionVertices},
-        L<:AbstractVector{<:Integer},
+        C<:AbstractVector{RealSpaceCoordinates},
 }
         coordinates::C
-        labels::L
-        translational_labels::L
-        rev_connections::T
 end
 
 function RealSpaceLattice(nlce_tiling::Tiling, max_order::Int)
 
 end
 
-pairwise_distance(lattice::RealSpaceLattice) = pairwise(euclidean, lattice.coords, dims=1)
-coords(lattice::RealSpaceLattice) = eachrow(lattice.coords)
-labels(lattice::RealSpaceLattice) = lattice.labels
-translational_labels(lattice::RealSpaceLattice) = lattice.translational_labels
+Base.getindex(lattice::RealSpaceLattice, ind::Int) = lattice.coordinates[ind]
+
+pairwise_distance(lattice::RealSpaceLattice) = pairwise(euclidean, vcat(coords(lattice)), dims=1)
+coords(lattice::RealSpaceLattice) = coord.(lattice.coordinates)
+labels(lattice::RealSpaceLattice) = label.(lattice.coordinates)
+translational_labels(lattice::RealSpaceLattice) = translational_label.(lattice.coordinates)
+
+exp_vertices(lattice::RealSpaceLattice, ind::Int) = exp_vertices(lattice[ind])
 
 function matching_exp_vertex(lattice::RealSpaceLattice, i::Int, j::Int)
         # Sometimes, there are dangling bonds in the lattice, however, these are usually
         # far out enough to where you will never expand into then, so they can safely
         # be set to 0 and not be dealt with
-        v = intersect(lattice.rev_connections[i], lattice.rev_connections[j])
+        v = intersect(exp_vertices(lattice, i), exp_vertices(lattice, j))
         if isempty(v)
                 return zero(eltype(v))
         end
--- src/Tiling/LabeledCoordinates.jl
+++ src/Tiling/LabeledCoordinates.jl
@@ -1,7 +1,46 @@
-struct LabeledCoordinate{C<:AbstractMatrix,L<:Integer}
+struct RealSpaceVertex{C<:AbstractArray,L<:Integer,E<:ExpansionVertices}
         coord::C
         label::L
         translational_label::L
+        expansion_vertices::E
+end
+
+function merge(l1::LabeledCoordinate, l2::LabeledCoordinate)
+
+        # label for these two should be the same, if they are
+        # related by translation, translational label just
+        # devolves down to lowest possible label for all
+        # coordinates
+        LabeledCoordinate(
+                l1.coord,
+                l1.label,
+                min(l1.translational_label, l2.translational_label),
+                vcat(l1.expansion_vertex, l2.expansion_vertex)
+        )
+
+end
+
+function merge_coords(ls::AbstractSet{LabeledCoordinate}, l::LabeledCoordinate)
+        # Should not encounter the exact same labeled coordinate twice,
+        # truly bending the system with this definition of equality here
+        if l in ls
+                return push!(merge(pop!(ls, l), l))
+        end
+
+        push!(ls, l)
+end
+
+function reduce_coords(ls::AbstractVector{LabeledCoordinate})
+        sort(collect(foldl(merge_coords, ls, init=Set{LabeledCoordinate}())))
 end
 
 
+dimension(l::LabeledCoordinate) = length(l.coord)
+coord(l::LabeledCoordinate) = l.coord
+label(l::LabeledCoordinate) = l.label
+translation_label(l::LabeledCoordinate) = l.translational_label
+expansion_vertex(l::LabeledCoordinate) = l.expansion_vertex
+
+Base.hash(l::LabeledCoordinate, h::UInt) = hash(l.coord, h)
+
+Base.isequal(l1::LabeledCoordinate, l2::LabeledCoordinate) = (l1.coord == l2.coord)
